75c864a451514ae645f6deacf1f9a803163eaaa5
Rodrigo P <rodrigo.pulido@cosmicgo.co>
2019-03-07 00:04:12 -0500

    Googlemaps
    se añade google maps, se trabaja con marcadores editados (agregar/quitar), se añaden boton pra buscar la posicion actual usando GPS

--- Ejercicio/app/src/main/java/com/example/rodpro/ejercicio_intent/MapsActivity.java
+++ Ejercicio/app/src/main/java/com/example/rodpro/ejercicio_intent/MapsActivity.java
@@ -1,21 +1,34 @@
 package com.example.rodpro.ejercicio_intent;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
-    private Button map, terr, hibr, sat, non;
+    private Button map, terr, hibr, sat, non, inter, buscar;
+    private Marker marcador;
+    double lat = 0.0, lng = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +39,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         hibr = findViewById(R.id.bhibrido);
         sat = findViewById(R.id.bsatelite);
         non = findViewById(R.id.bnone);
+        inter = findViewById(R.id.binterior);
+        buscar = findViewById(R.id.bBuscar);
 
         map.setOnClickListener(this);
         terr.setOnClickListener(this);
         hibr.setOnClickListener(this);
         sat.setOnClickListener(this);
         non.setOnClickListener(this);
+        inter.setOnClickListener(this);
+        buscar.setOnClickListener(this);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -39,47 +56,113 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Sogamoso and move the camera
+        LatLng sogamoso = new LatLng(5.7133, -72.930787);
+        mMap.addMarker(new MarkerOptions().position(sogamoso).title("Sogamoso Boyaca"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sogamoso, 12));
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                mMap.addMarker(new MarkerOptions()
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.nube))
+                        .anchor(0.0f, 1.0f)
+                        .position(latLng));
+
+
+            }
+        });
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Toast.makeText(getApplicationContext(), "nubesita", Toast.LENGTH_SHORT).show();
+                marker.remove();
+                return false;
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.bmapa:
                 mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            break;
+                break;
             case R.id.bterreno:
                 mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-            break;
+                break;
             case R.id.bhibrido:
                 mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-            break;
+                break;
             case R.id.bsatelite:
                 mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-            break;
+                break;
             case R.id.bnone:
                 mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
-            break;
+                break;
+            case R.id.binterior:
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-33.86997, 151.2089), 18));
+                break;
+            case R.id.bBuscar:
+                miUbicacion();
+                break;
             default:
                 break;
         }
 
+    }
+
+    public void agregarMarcador(double lat, double lng) {
+        LatLng coordenadas = new LatLng(lat, lng);
+        CameraUpdate miUbicacion = CameraUpdateFactory.newLatLngZoom(coordenadas, 16);
+        if (marcador != null) marcador.remove();
+        //mMap.addMarker(new MarkerOptions().position(sogamoso).title("Sogamoso Boyaca"));
+        marcador = mMap.addMarker(new MarkerOptions().position(coordenadas).title("Yo!!").
+                icon(BitmapDescriptorFactory.fromResource(R.drawable.me)));
+        mMap.animateCamera(miUbicacion);
+    }
+
+    public void actualizarUbicacion(Location location) {
+        if (location != null) {
+            lat = location.getLatitude();
+            lng = location.getLongitude();
+            agregarMarcador(lat, lng);
+        }
+    }
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            actualizarUbicacion(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    public void miUbicacion() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        actualizarUbicacion(location);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,100000,0,locationListener);
     }
 }
