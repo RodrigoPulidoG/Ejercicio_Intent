ec6e05711dbe91b5d6b80a2c1ce6c84cecc351da
Rodrigo P <rodrigo.pulido@cosmicgo.co>
2019-09-10 13:41:06 -0500

    Refactor AndroidX

--- Ejercicio/app/src/main/java/com/example/rodpro/ejercicio_intent/MainActivity.java
+++ Ejercicio/app/src/main/java/com/example/rodpro/ejercicio_intent/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.rodpro.ejercicio_intent;
 
 import android.content.Intent;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
--- Ejercicio/app/src/main/java/com/example/rodpro/ejercicio_intent/ThirdActivity.java
+++ Ejercicio/app/src/main/java/com/example/rodpro/ejercicio_intent/ThirdActivity.java
@@ -1,16 +1,13 @@
 package com.example.rodpro.ejercicio_intent;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
